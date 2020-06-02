325ea21104a4aefd24e45806cb9ca4da0fc6943c
Carlos Yepes <carlos.d.yepes.22@dartmouth.edu>
2020-06-02 13:44:53 -0400

    help modal

--- src/components/help.js
+++ src/components/help.js
@@ -6,6 +6,7 @@ const useStyles = {
   header: {
     display: 'flex',
     flexDirection: 'column',
+    padding: '10px',
   },
   modal: {
     display: 'flex',
@@ -27,14 +28,12 @@ const useStyles = {
     background: 'linear-gradient(48deg, rgba(0,42,50,1) 39%, rgba(39,142,148,1) 52%, rgba(0,42,50,1) 70%);',
     borderRadius: '10px',
   },
+  info: { padding: '20px', textAlign: 'center', fontSize: '20px' },
+  theX: {
+    fontSize: '30px', fontWeight: '700', cursor: 'pointer', alignSelf: 'flex-end',
+  },
   modalDisplay: { opacity: '1', transform: 'scale(1)' },
   modalHide: { opacity: '0', transform: 'scale(0)' },
-  theX: {
-    cursor: 'pointer',
-    alignSelf: 'flex-end',
-    paddingRight: '15px',
-    paddingTop: '15px',
-  },
 };
 
 const HelpModal = (props) => {
@@ -44,12 +43,21 @@ const HelpModal = (props) => {
     <div className={showClassName}>
       <div className={props.classes.content}>
         <div className={props.classes.header}>
-          <span>How to play:</span>
           <span className={props.classes.theX} onClick={props.closeModal}>&times;</span>
         </div>
-        <div>
-          <p>This is the help section</p>
-          <p>Reeeeeeeeeeee</p>
+        <div className={props.classes.info}>
+          <p>To start off, each player will randomly be assigned a role. You will either be in the Resistance, or be a Spy. A random person will be selected
+            as the Team leader, and is in charge of selecting the people who will go on the mission. Each mission requires a certain amount of players
+            indicated by the mission table on the board and a majority vote.
+          </p>
+          <p>For a mission to succeed, it requires every single person on the team to vote PASS, otherwise the mission will fail. The board also contains the vote track,
+            which will display the number of failed votes for the current mission, it will reset on a successful mission vote. If everyone fails to pass a team 5 times
+            for a single mission, the game is over and the Spys win! (You cant win a fight if youre taking 3000 years to make a decision)
+          </p>
+          <span>If you are in the <strong>Resistance:</strong></span>
+          <p>You win by passing 3 missions!</p>
+          <span>If you are a <strong>Spy:</strong></span>
+          <p> You win by failing 3 missions, OR if the Resistance is unable to organize a mission team at any point in the game (5 failed votes on a single mission.)</p>
         </div>
       </div>
     </div>
