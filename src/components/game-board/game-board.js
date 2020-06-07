8da5f9bde25d085b6239ee93291c6f2bb618b792
Will Baxley <will.baxley27@gmail.com>
2020-06-07 15:56:25 -0400

    made 'join' and 'create' buttons unavailable until all fields  are filled

--- src/components/game-board/game-board.js
+++ src/components/game-board/game-board.js
@@ -90,7 +90,7 @@ class GameBoard extends Component {
             {missions}
           </div>
           <div className="current-round">
-            Attempt to approve team: <span>{this.props.currentRound}</span>
+            Attempt to approve team: {this.props.currentRound} (of 5)
           </div>
           <div className="player-cards">
             {players}
