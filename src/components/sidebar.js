627359a05d27dc1f775a0c28b53ba70fffb4d51a
Will Baxley <will.baxley27@gmail.com>
2020-06-07 14:35:28 -0400

    made the directions viewable from the sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,13 +2,23 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Chat from './chat';
+import DirectionsModal from './directions-modal';
 
 class SideBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       show: true,
+      showDirections: false,
     };
+  }
+
+  openDirections = () => {
+    this.setState({ showDirections: true });
+  }
+
+  closeDirections = () => {
+    this.setState({ showDirections: false });
   }
 
   toggleCollapse = () => {
@@ -25,6 +35,7 @@ class SideBar extends Component {
         <div className="sidebar-material">
           <div className="sidebar-buttons">
             <Button variant="secondary" className="directions-button" onClick={this.openDirections}>Directions</Button>
+            <Button variant="secondary" className="discord-button" onClick={this.thisMethodDoesNotExist}>Join Discord</Button>
           </div>
           <Chat />
         </div>
@@ -36,6 +47,7 @@ class SideBar extends Component {
   render() {
     return (
       <div className="sidebar-container">
+        <DirectionsModal show={this.state.showDirections} closeModal={this.closeDirections} />
         {this.renderMaterial()}
         <div role="button" tabIndex="0" className="sidebar-toggle" onClick={this.toggleCollapse}>
           {this.state.show && <i className="fas fa-caret-left" />}
