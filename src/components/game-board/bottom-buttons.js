c4fb98dbb8194006a967a5a93c15cb43173a4ea8
Will Baxley <will.baxley27@gmail.com>
2020-06-09 15:11:10 -0400

    changed 'SUCCESS' message to 'SUCCEED'

--- src/components/game-board/bottom-buttons.js
+++ src/components/game-board/bottom-buttons.js
@@ -99,7 +99,7 @@ class BottomButtons extends Component {
     if (this.props.selectedPlayers.includes(this.props.playerID) && this.props.acted === false) {
       return (
         <div className="horizontal-flex-center bottom-navigation">
-          <Button variant="primary" onClick={() => this.submitMissionVote('SUCCESS')}>
+          <Button variant="primary" onClick={() => this.submitMissionVote('SUCCEED')}>
             Succeed
           </Button>
           <Button variant="danger" className="negative" onClick={() => this.submitMissionVote('FAIL')}>
